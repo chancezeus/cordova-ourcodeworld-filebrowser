190e2653d1470cd59bf41c0717bf449ec49e3e29
Mark van Beek <flipvb@gmail.com>
2017-02-05 20:06:02 +0100

    Remove unneeded activity Fall back to default style Set label to @string/app_name

--- src/android/Filebrowser.java
+++ src/android/Filebrowser.java
@@ -1,35 +1,75 @@
 package com.ourcodeworld.plugins.filebrowser;
 
-import org.apache.cordova.*;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.json.JSONException;
-import android.content.Intent;
+import android.app.Activity;
 import android.content.ActivityNotFoundException;
+import android.content.ClipData;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import com.nononsenseapps.filepicker.FilePickerActivity;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import android.os.Bundle;
+import java.util.ArrayList;
 
 public class Filebrowser extends CordovaPlugin {
-    private CallbackContext PUBLIC_CALLBACKS = null;
+    private static int FILE_PICKER_CODE = 0x123987;
+    private CallbackContext callback = null;
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-        PUBLIC_CALLBACKS = callbackContext;
-        final JSONObject arg_object = data.getJSONObject(0);
+        JSONObject arg_object = data.getJSONObject(0);
         String startDirectory = arg_object.getString("start_directory");
+        callback = callbackContext;
 
         try {
-            Intent intent = new Intent("com.ourcodeworld.plugins.filebrowser.DialogShowPicker");
-            intent.putExtra("action", action);
-            intent.putExtra("start_directory", startDirectory);
-            cordova.startActivityForResult((CordovaPlugin) this, intent, 0);
+            Context context = cordova.getActivity().getApplicationContext();
+            Intent intent = new Intent(context, FilePickerActivity.class);
+
+            if (startDirectory.equals("default")) {
+                intent.putExtra(FilePickerActivity.EXTRA_START_PATH, Environment.getExternalStorageDirectory().getPath());
+            } else {
+                intent.putExtra(FilePickerActivity.EXTRA_START_PATH, startDirectory);
+            }
+
+            // Start single filepicker
+            if (action.equals("showPicker")) {
+                intent.putExtra(FilePickerActivity.EXTRA_ALLOW_MULTIPLE, false);
+                intent.putExtra(FilePickerActivity.EXTRA_MODE, FilePickerActivity.MODE_FILE);
+                // Start multi filepicker
+            } else if (action.equals("showMultiFilepicker")) {
+                intent.putExtra(FilePickerActivity.EXTRA_ALLOW_MULTIPLE, true);
+                intent.putExtra(FilePickerActivity.EXTRA_MODE, FilePickerActivity.MODE_FILE);
+                // Start folder (dir) picker
+            } else if (action.equals("showFolderpicker")) {
+                intent.putExtra(FilePickerActivity.EXTRA_ALLOW_MULTIPLE, false);
+                intent.putExtra(FilePickerActivity.EXTRA_MODE, FilePickerActivity.MODE_DIR);
+                // Start multi folder (dir) picker
+            } else if (action.equals("showMultiFolderpicker")) {
+                intent.putExtra(FilePickerActivity.EXTRA_ALLOW_MULTIPLE, true);
+                intent.putExtra(FilePickerActivity.EXTRA_MODE, FilePickerActivity.MODE_DIR);
+            } else if (action.equals("showMixedPicker")) {
+                intent.putExtra(FilePickerActivity.EXTRA_ALLOW_MULTIPLE, true);
+                intent.putExtra(FilePickerActivity.EXTRA_MODE, FilePickerActivity.MODE_FILE_AND_DIR);
+            } else if (action.equals("showCreatefile")) {
+                intent.putExtra(FilePickerActivity.EXTRA_MODE, FilePickerActivity.MODE_NEW_FILE);
+                intent.putExtra(FilePickerActivity.EXTRA_ALLOW_CREATE_DIR, true);
+            }
+
+            cordova.startActivityForResult((CordovaPlugin) this, intent, FILE_PICKER_CODE);
         } catch (ActivityNotFoundException e) {
-            PluginResult errorResults = new PluginResult(PluginResult.Status.ERROR, "Cannot find activity com.ourcodeworld.plugins.filebrowser.DialogShowPicker");
+            PluginResult errorResults = new PluginResult(PluginResult.Status.ERROR, "Could not start FilePickerActivity");
             errorResults.setKeepCallback(true);
-            PUBLIC_CALLBACKS.sendPluginResult(errorResults);
+            callback.sendPluginResult(errorResults);
         }
 
-        PluginResult pluginResult = new  PluginResult(PluginResult.Status.NO_RESULT);
+        PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
         pluginResult.setKeepCallback(true);
 
         return true;
@@ -37,16 +77,41 @@ public class Filebrowser extends CordovaPlugin {
 
     @Override
     public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
-        if(resultCode == cordova.getActivity().RESULT_OK){
-            Bundle extras = data.getExtras();
-            String resultData = extras.getString("information");
+        // Retrieve file, folders paths
+        if (requestCode == FILE_PICKER_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                JSONArray jsonArray = new JSONArray();
 
-            PluginResult resultado = new PluginResult(PluginResult.Status.OK, resultData);
-            resultado.setKeepCallback(true);
-            PUBLIC_CALLBACKS.sendPluginResult(resultado);
-            return;
+                if (data.getBooleanExtra(FilePickerActivity.EXTRA_ALLOW_MULTIPLE, false)) {
+                    // For JellyBean and above
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                        ClipData clip = data.getClipData();
+
+                        if (clip != null) {
+                            for (int i = 0; i < clip.getItemCount(); i++) {
+                                jsonArray.put(clip.getItemAt(i).getUri().toString());
+                            }
+                        }
+                        // For Ice Cream Sandwich
+                    } else {
+                        ArrayList<String> paths = data.getStringArrayListExtra(FilePickerActivity.EXTRA_PATHS);
+
+                        if (paths != null) {
+                            for (String path : paths) {
+                                jsonArray.put(Uri.parse(path).toString());
+                            }
+                        }
+                    }
+                } else {
+                    jsonArray.put(data.getData().toString());
+                }
+
+                PluginResult result = new PluginResult(PluginResult.Status.OK, jsonArray);
+                callback.sendPluginResult(result);
+            } else {
+                PluginResult result = new PluginResult(PluginResult.Status.ERROR, "Error selecting file");
+                callback.sendPluginResult(result);
+            }
         }
-
-        super.onActivityResult(requestCode, resultCode, data);
     }
 }
